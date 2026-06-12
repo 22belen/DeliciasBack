aad8329e81c65bf486d8ceb4ce7f8e4150ed915a
22belen <belen.fleitas@gmail.com>
2026-06-12 19:55:09 -0300

    Revert "login"
    This reverts commit a1384eea20c8729df3b07940f4c54818e1c42eb4.

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize("delicias_db", "root", "Root", {
+const sequelize = new Sequelize("delicias_db", "root", "root", {
   host: "127.0.0.1",
   port: 3306,
   dialect: "mysql",
